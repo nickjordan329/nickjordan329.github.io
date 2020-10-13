1c8f3c03aa5a0e63a0022bc087813245136dd5ba
nickjordan329 <71334286+nickjordan329@users.noreply.github.com>
2020-10-13 15:55:41 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -26,7 +26,7 @@ elem[19]=document.getElementById("totals");
 // console.log(elem.children[4].children[0]); 
 
 // assign a function to the + button
-elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
+elem[1].children[4].children[0].onclick = function(){add1(elem[1]);} function(){totals
 elem[2].children[4].children[0].onclick = function(){add1(elem[2]);};
 elem[3].children[4].children[0].onclick = function(){add1(elem[3]);};
 elem[4].children[4].children[0].onclick = function(){add1(elem[4]);};
@@ -65,11 +65,35 @@ elem[16].children[4].children[1].onclick = function(){sub1(elem[16]);};
 elem[17].children[4].children[1].onclick = function(){sub1(elem[17]);};
 elem[18].children[4].children[1].onclick = function(){sub1(elem[18]);};
 
+//assign a function to the clear button
+elem[1].children[4].children[2].onclick = function(){clear(elem[1]);};
+elem[2].children[4].children[2].onclick = function(){clear(elem[2]);};
+elem[3].children[4].children[2].onclick = function(){clear(elem[3]);};
+elem[4].children[4].children[2].onclick = function(){clear(elem[4]);};
+elem[5].children[4].children[2].onclick = function(){clear(elem[5]);};
+elem[6].children[4].children[2].onclick = function(){clear(elem[6]);};
+elem[7].children[4].children[2].onclick = function(){clear(elem[7]);};
+elem[8].children[4].children[2].onclick = function(){clear(elem[8]);};
+elem[9].children[4].children[2].onclick = function(){clear(elem[9]);};
+elem[10].children[4].children[2].onclick = function(){clear(elem[10]);};
+elem[11].children[4].children[2].onclick = function(){clear(elem[11]);};
+elem[12].children[4].children[2].onclick = function(){clear(elem[12]);};
+elem[13].children[4].children[2].onclick = function(){clear(elem[13]);};
+elem[14].children[4].children[2].onclick = function(){clear(elem[14]);};
+elem[15].children[4].children[2].onclick = function(){clear(elem[15]);};
+elem[16].children[4].children[2].onclick = function(){clear(elem[16]);};
+elem[17].children[4].children[2].onclick = function(){clear(elem[17]);};
+elem[18].children[4].children[2].onclick = function(){clear(elem[18]);};
+
 
 // create an "add1" function
 function add1 (elem) {
-  if(elem.children[2].innerHTML == "-") 
+  if(elem.children[2].innerHTML == "-"){	  
     elem.children[2].innerHTML = "1";
+	let par = elem.children[1].innerHTML;
+	par = Number.parseInt(par);
+	elem.children[3].innerHTML=currentScore - par;
+  }
   else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
@@ -81,6 +105,7 @@ function add1 (elem) {
 	elem.children[3].innerHTML=currentScore - par;
   }
 }
+//create sub1 function
 function sub1 (elem) {
   if( elem.children[2].innerHTML == "-" || elem.children[2].innerHTML== "1"){
     elem.children[2].innerHTML = "-";
@@ -99,6 +124,33 @@ function sub1 (elem) {
 		  elem.children[3].innerHTML=currentScore - par;
 	  }
   }
+}
+//create clear function
+function clear(elem){
+	elem.children[2].innerHTML = "-";
+    elem.children[3].innerHTML="-";
+}
+
+//create function to to do totals
+function totals(elem){
+    let overTotal = 0;
+    let scoreTotal = 0;
+  for(let i=0;i<elem.length;i++)
+  {
+    if(elem[i].children[2].innerHTML=="-")
+      scoreTotal+=0;
+    else{
+      let currScore = Number.parseInt(elem[i].children[2].innerHTML);
+      scoreTotal+=currScore;
+    }
+    if(elem[i].children[3].innerHTML=="-")
+      overTotal+=0;
+    else{
+      let currOver = Number.parseInt(elem[i].children[3].innerHTML);
+      scoreTotal+=currOver;
+  }
+    elem[19].children[2].innerHTML = scoreTotal;
+    elem[19].children[3].innerHTML = overTotal;
 }
 
 //REWORK AND MAKE 4 new functions
